9b8c1384b1397f11b3da2aa0db99d46a4a74e788
Tegar Satria Alam <117506176+tegarsatriaalam@users.noreply.github.com>
2023-05-11 17:45:07 +0700

    Add files via upload

---
+++ harberweb.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#btnsearch").click(function(e){
+        var txtInput = $("#srchInv").val();
+        $("#infoinvaccr").html("Nama Anda : "+txtInput);
+        $.ajax({
+            type:"POST",
+            utl:"/assets/scripts/ajax/getcurl.php",
+            data:"",
+            success: function (response){
+                $("#infoinvaccr").html("<br/>Response</br>"+response);
+            }
+        });
+    });
+});
---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+</head>
+<?php
+require_once('./assets/requires/config.php');
+require_once('./assets/requires/header.php');
+?>
+
+<div class="container">
+	<div id="jdlaccr" style="font-size:36px;color:brown;">Harber Web</div>
+	<div class="card shadow-sm mb-5 bg-black rounded">
+		<div class="card-body" >
+			<h5 class="card-title">input Name:</h5>
+			<p class="card-text"><input type="test" class="input input-lg" style="width:100%;" id="srchInv"></p>
+</div>
+<div class="card-footer">
+	<button class="btn btn-primary" id="btnsearch">Submit</button>
+           </div>
+        </div>
+        <div id="infoinvaccr"></div>
+    </div>
+</html>
+<script src="./assets/scripts/js/harberweb.js"></script>	
+	
